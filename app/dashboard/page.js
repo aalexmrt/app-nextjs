c1db50d93bb04e6799745e59dabf9a81229cb493
Alex Martinez <alexmartinez.mm98@gmail.com>
2024-09-28 16:24:11 -0500

    wip

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,10 @@
 import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
 
 export default withPageAuthRequired(
-  async function Home() {
+  async function Profile() {
     const { user } = await getSession();
     return <div>Hello {user.name}</div>;
   },
   { returnTo: "/" }
 );
+// You need to provide a `returnTo` since Server Components aren't aware of the page's URL
