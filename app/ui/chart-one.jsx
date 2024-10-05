55c724db4136bc8045259d3e5e037ce35d8662a9
Alex Martinez <alexmartinez.mm98@gmail.com>
2024-10-05 16:44:26 -0500

    protect all routes, and add specific authorization to hidden-dashboard

--- app/ui/chart-one.jsx
+++ app/ui/chart-one.jsx
@@ -7,44 +7,26 @@ const dataset = [
   {
     month: "Apr",
     revenue: 94.7,
-    activeCustomers: 3626,
-    orders: 16606,
-    profitMargin: 48,
   },
   {
     month: "May",
     revenue: 85.3,
-    activeCustomers: 3700,
-    orders: 15000,
-    profitMargin: 47,
   },
   {
     month: "Jun",
     revenue: 70.2,
-    activeCustomers: 3780,
-    orders: 14000,
-    profitMargin: 47.5,
   },
   {
     month: "Jul",
     revenue: 60.5,
-    activeCustomers: 3850,
-    orders: 12000,
-    profitMargin: 48,
   },
   {
     month: "Aug",
     revenue: 50.1,
-    activeCustomers: 4000,
-    orders: 10000,
-    profitMargin: 48.5,
   },
   {
     month: "Sep",
     revenue: 44.5,
-    activeCustomers: 4118,
-    orders: 7961,
-    profitMargin: 48,
   },
 ];
 
@@ -68,6 +50,8 @@ export default function ChartOne() {
           series={[
             {
               dataKey: "revenue",
+              stroke: "#00B5E2",
+              fill: "#00B5E2",
             },
           ]}
           {...chartSetting}
--- middleware.js
+++ middleware.js
@@ -1,10 +1,31 @@
 import { NextResponse } from "next/server";
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
-
-// export default withMiddlewareAuthRequired(async function middleware(request) {
-//   return NextResponse.next();
-// });
-
+import * as jose from "jose";
 export default async function middleware(request) {
-  return NextResponse.next();
+  const { pathname } = request.nextUrl;
+
+  // Bypass authentication for Puppeteer-specific routes
+  if (pathname.startsWith("/hidden-dashboard")) {
+    const token = request.headers.get("Authorization");
+
+    // Check with auth0 that the token provided is valid with jose
+    const jwks = jose.createRemoteJWKSet(new URL(process.env.AUTH0_JWKS_URI));
+
+    try {
+      // Verify the given token
+      await jose.jwtVerify(token.replace("Bearer ", ""), jwks);
+      return NextResponse.next();
+    } catch (e) {
+      return new NextResponse(
+        JSON.stringify({
+          success: false,
+          message: "Authentication failed: Token could not be verified",
+        }),
+        { status: 401, headers: { "content-type": "application/json" } }
+      );
+    }
+  }
+
+  // Apply Auth0 middleware for all other routes
+  return withMiddlewareAuthRequired()(request);
 }
